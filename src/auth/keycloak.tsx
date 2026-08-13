ab013753f4ff6f75aed91b555bf021f80bb633d4
enri123 <88102659+enri123@users.noreply.github.com>
2026-08-13 14:29:31 +0200

    Cambios en los ficheros

--- src/auth/keycloak.tsx
+++
@@ -1,9 +0,0 @@
-import Keycloak from 'keycloak-js';
-import { KEYCLOAK_URL, KEYCLOAK_REALM } from '../const';
-const keycloak = new Keycloak({
-  url: KEYCLOAK_URL,
-  realm: KEYCLOAK_REALM,
-  clientId: 'react-app-client',
-});
-
-export default keycloak;
--- src/const.tsx
+++
@@ -1,5 +0,0 @@
-// Contantes que usamos en el proyecto, se podrían añadir algunas mas, pero al ser un proyecto pequeño,
-// pues realmente tampoco importa mucho
-
-export const KEYCLOAK_URL = 'http://localhost:8080';
-export const KEYCLOAK_REALM = 'reino-infodp';
